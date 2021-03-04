f39a72d7de126c3e0a8c591339f44efd2b2764cf
Peter Harrison <peter.harrison@helicron.net>
2021-03-04 23:05:50 +0000

    update license headers in new files

--- read-number.cpp
+++ read-number.cpp
@@ -1,34 +1,38 @@
 /*
- * File: read-number.cpp                                                                 *
- * Project: ukmarsey                                                                     *
- * File Created: Thursday, 4th March 2021 2:36:40 pm                                     *
- * Author: Peter Harrison                                                                *
- * -----                                                                                 *
- * Last Modified: Thursday, 4th March 2021 3:08:07 pm                                    *
- * Modified By: Peter Harrison                                                           *
- * -----                                                                                 *
- * MIT License                                                                           *
- *                                                                                       *
- * Copyright (c) 2021 Peter Harrison                                                     *
- *                                                                                       *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of       *
- * this software and associated documentation files (the "Software"), to deal in         *
- * the Software without restriction, including without limitation the rights to          *
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies         *
- * of the Software, and to permit persons to whom the Software is furnished to do        *
- * so, subject to the following conditions:                                              *
- *                                                                                       *
- * The above copyright notice and this permission notice shall be included in all        *
- * copies or substantial portions of the Software.                                       *
- *                                                                                       *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR            *
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,              *
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE           *
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER                *
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,         *
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE         *
- * SOFTWARE.                                                                             *
- */
+ * File: read-number.cpp
+ * Project: ukmarsey
+ * File Created: Thursday, 4th March 2021 2:36:40 pm
+ *
+ *   ukmarsey is a machine and human command-based Robot Low-level I/O platform initially targetting UKMARSBot
+ *   For more information see:
+ *       https://github.com/robzed/ukmarsey
+ *       https://ukmars.org/
+ *       https://github.com/ukmars/ukmarsbot
+ *       https://github.com/robzed/pizero_for_ukmarsbot
+ *
+ *  MIT License
+ *
+ *  Copyright (c) 2020-2021 Rob Probin & Peter Harrison
+ *  Copyright (c) 2019-2021 UK Micromouse and Robotics Society
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+*/
 
 #include "read-number.h"
 #include "public.h"
